2ae73aa60602a7acd036564e20dc103c3963efad
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-05-28 14:45:55 -0300

    Improving the method ImprimirTabuleiro() on Tela class

--- Xadrez-Console/Program.cs
+++ Xadrez-Console/Program.cs
@@ -14,7 +14,7 @@ namespace Xadrez_Console
                 Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
                 tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(0, 0));
-                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Preta), new Posicao(1, 3));
+                tabuleiro.ColocarPeca(new Torre(tabuleiro, Cor.Branca), new Posicao(1, 3));
                 tabuleiro.ColocarPeca(new Rei(tabuleiro, Cor.Preta), new Posicao(0, 2));
 
                 Tela.ImprimirTabuleiro(tabuleiro);
