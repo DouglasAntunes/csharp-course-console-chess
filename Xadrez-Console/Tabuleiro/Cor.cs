30382f5dc0adce88d11c8129b06c80cdf6df6f76
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-05-28 02:17:09 +0000

    Initial Board Implementation

---
+++ Xadrez-Console/Tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Verde,
+        Laranja,
+    }
+}
---
+++ Xadrez-Console/Tabuleiro/Peca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QuantidadeDeMovimentos { get; protected set; } = 0;
+        public Tabuleiro Tabuleiro { get; protected set; }
+
+        public Peca(Posicao posicao, Tabuleiro tabuleiro, Cor cor)
+        {
+            Posicao = posicao;
+            Tabuleiro = tabuleiro;
+            Cor = cor;
+        }
+    }
+}
